badd898e35dcc983178f0752e5d7413fa9d4ee1a
Mohab2000 <mohabahmed20005@gmail.com>
2023-10-26 14:09:28 +0300

    Connecting to mongo

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,7 @@ exports.getAllTours = (req, res) => {
 };
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id == id);
+  const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
     data: { tour: tour },
@@ -38,12 +38,12 @@ exports.getTour = (req, res) => {
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object({ id: newId }, req.body);
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    () => {
       res.status(201).json({
         status: 'success',
         data: {
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('DB Connection Successful!');
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
